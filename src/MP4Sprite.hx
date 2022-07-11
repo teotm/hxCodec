9778d5a37fb3cae3d8ebf9fd170a72ad46a01ecf
KolczatkowyToNieJestPrzymiotnik <84466597+KolczatkowyToNieJestPrzymiotnik@users.noreply.github.com>
2022-07-11 11:21:11 +0200

    bruh mac support

--- src/MP4Sprite.hx
+++ src/MP4Sprite.hx
@@ -2,30 +2,26 @@ package;
 
 import flixel.FlxSprite;
 
-class MP4Sprite extends FlxSprite
-{
+class MP4Sprite extends FlxSprite {
 	public var readyCallback:Void->Void = null;
 	public var finishCallback:Void->Void = null;
 
 	public var bitmap:MP4Handler;
 
-	public function new(x:Float = 0, y:Float = 0)
-	{
+	public function new(x:Float = 0, y:Float = 0) {
 		super(x, y);
 
 		bitmap = new MP4Handler();
 		bitmap.visible = false;
 
-		bitmap.readyCallback = function()
-		{
+		bitmap.readyCallback = function() {
 			loadGraphic(bitmap.bitmapData);
 
 			if (readyCallback != null)
 				readyCallback();
 		}
 
-		bitmap.finishCallback = function()
-		{
+		bitmap.finishCallback = function() {
 			if (finishCallback != null)
 				finishCallback();
 
@@ -40,8 +36,7 @@ class MP4Sprite extends FlxSprite
 	 * @param haccelerated if you want the hardware to accelerated for the video.
 	 * @param pauseMusic Pause music until done video.
 	 */
-	public function playVideo(path:String, loop:Bool = false, haccelerated:Bool = true, pauseMusic:Bool = false):Void
-	{
+	public function playVideo(path:String, loop:Bool = false, haccelerated:Bool = true, pauseMusic:Bool = false):Void {
 		bitmap.playVideo(path, loop, haccelerated, pauseMusic);
 	}
 }
